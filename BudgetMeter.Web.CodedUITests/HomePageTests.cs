cf189e4e9bad408181d1164a15eb9b166b71571b
Raf Van Baelen <rafvanbaelen@gmail.com>
2014-10-03 14:10:56 +0200

    Initial project including Selenium WebDriver test

---
+++ BudgetMeter.Web.CodedUITests/HomePageTests.cs
@@ -0,0 +1,21 @@
+﻿namespace BudgetMeter.Web.CodedUITests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using OpenQA.Selenium.IE;
+
+    [TestClass]
+    public class HomePageTests
+    {
+        [TestMethod]
+        public void HasCorrectTitle()
+        {
+            var driver = new InternetExplorerDriver();
+
+            driver.Navigate().GoToUrl("http://localhost:50765/");
+
+            Assert.AreEqual("BudgetMeter - Home", driver.Title);
+
+            driver.Quit();
+        }
+    }
+}
---
+++ BudgetMeter.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BudgetMeter.Web.MvcApplication" Language="C#" %>
